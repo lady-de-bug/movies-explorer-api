b55e6542aef14dc8ae6998115df675999962a814
Elena Kondrateva <epkondrateva@yandex.ru>
2023-08-22 22:48:53 +0300

    Change Port to 4000

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const router = require('./routes');
 const error = require('./middlewares/error');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 4000 } = process.env;
 mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
   useNewUrlParser: true,
 });
