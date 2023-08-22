b0912f52cf4e91811e57a1ad5b56f249e6e0df5d
Elena Kondrateva <epkondrateva@yandex.ru>
2023-08-22 22:26:34 +0300

    Add main functions

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const cors = require('cors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const router = require('./routes');
+const error = require('./middlewares/error');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(requestLogger);
+
+app.use(router);
+
+app.use(errorLogger);
+app.use(errors());
+app.use(error);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
